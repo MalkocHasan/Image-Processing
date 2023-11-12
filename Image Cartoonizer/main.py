8e8ad8a18aa50cb48c32bf11f43f7a058ee64662
b2200356826 <b2200356826@cs.hacettepe.edu.tr>
2023-11-12 13:25:42 +0300

    final results

--- Image Cartoonizer/main.py
+++ Image Cartoonizer/main.py
@@ -4,32 +4,35 @@ import cv2
 import os
 
 #parameters
-sigma = 1.2
+sigma = 1.1
 k = 1.3
 threshold = 15
-quantization_level=8
+quantization_level=32
 input_image_path = "report/data/ankarakalesi.jpg" 
 
 def edgeDedection(input_image, sigma, k, threshold):
     input_image_gray = cv2.cvtColor(input_image, cv2.COLOR_RGB2GRAY)
-    
+
     # equation 1 and 2
     gaussian1 = cv2.GaussianBlur(input_image_gray, (0, 0), sigmaX=sigma)
     gaussian2 = cv2.GaussianBlur(input_image_gray, (0, 0), sigmaX=sigma * k)
     dog_filter = gaussian1 - gaussian2
     # equation 3
-    return cv2.threshold(dog_filter, threshold, 255, cv2.THRESH_BINARY)[1]
+    edges = cv2.threshold(dog_filter, threshold, 1, cv2.THRESH_BINARY)[1]
+
+    return edges.astype(np.uint8)
 
 
 def smoothImage(input_image, sigma):
     return cv2.GaussianBlur(input_image, (0, 0), sigmaX=sigma)
 
 
-def quantizeImage(smoothed_image, quantization_level):
+def quantizeImage(smoothed_image, quantization_levels):
+
     lab_image = cv2.cvtColor(smoothed_image, cv2.COLOR_RGB2LAB)
     L_channel = lab_image[:, :, 0]
 
-    quantized_L_channel = np.floor_divide(L_channel, 256 // quantization_level) * (256 // quantization_level)
+    quantized_L_channel = np.floor_divide(L_channel, 256 // quantization_levels) * (256 // quantization_levels)
 
     lab_image[:, :, 0] = quantized_L_channel
 
@@ -48,11 +51,10 @@ def cartoonizeImage(input_image):
     #edge dedection
     edges = edgeDedection(input_image, sigma, k, threshold)
     # Take the inverse of the estimated edges values.
-    inverted_edges_image = 255 - edges
-    inverted_edges_image = inverted_edges_image[:, :, np.newaxis]
+    inverted_edges_image = edges ^ 1
 
     # Multiply the inverted edges image with the quantized image for each channel.
-    return np.multiply(inverted_edges_image, quantized_image)
+    return quantized_image * inverted_edges_image[:, :, np.newaxis]
 
 
 
@@ -71,9 +73,11 @@ filename_without_extension, extension = os.path.splitext(filename)
 
 # Construct the output path based on the filename
 output_path = f"report/result/{filename_without_extension}_result{extension}"
-cv2.imwrite(output_path, cartoonized_image)
+plt.imsave(output_path, cartoonized_image, cmap='gray')
 print(f'Resulting image saved at: {output_path}')
 
+
+# Display the cartoon image
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 plt.imshow(input_image, cmap='gray')
@@ -82,7 +86,7 @@ plt.axis('off')
 
 
 plt.subplot(1, 2, 2)
-plt.imshow(cv2.cvtColor(cartoonized_image, cv2.COLOR_BGR2RGB))
+plt.imshow(cartoonized_image, cmap = 'gray')
 plt.title("Cartoon Image")
 plt.axis('off')
 plt.show()
